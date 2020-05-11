040b1c12e68d05076779b6e6ec485df99170102b
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2020-05-12 09:14:31 +1200

    Reorder stripe delete

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import { render } from "react-snapshot";
+import ReactDOM from "react-dom";
+// import { render } from "react-snapshot";
 import "./index.scss";
 import App from "./App";
 
 import * as serviceWorker from "./serviceWorker";
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.register();
